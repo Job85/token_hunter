501f7a4080c0f43b1df228325213f13705fecb82
Job85 <julianjernigan@ymail.com>
2022-05-06 11:51:23 -0400

    added GetToken logic

---
+++ backend/controllers/TokenController.js
@@ -0,0 +1,11 @@
+const { Token } = require('../models')
+
+const GetToken = async (req, res) => {
+    try {
+        const token = await Token.findAll()
+        console.log(token)
+        res.send(token)
+    } catch (error) {
+        throw error
+    }
+}
