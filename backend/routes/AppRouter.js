68c08a6c294787a74d832bd41c9ba081c31e9a44
Job85 <julianjernigan@ymail.com>
2022-05-06 15:24:15 -0400

    added LocationRouter

--- backend/routes/AppRouter.js
+++ backend/routes/AppRouter.js
@@ -1,6 +1,8 @@
 const Router = require('express').Router()
 const TokenRouter = require('./TokenRouter')
+const LocationRouter = require('./LocationRouter')
 
 Router.use('/token', TokenRouter)
+Router.use('/location', LocationRouter)
 
 module.exports = Router
