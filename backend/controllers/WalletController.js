c0fae2a29f0bb32fa92bfdef1a82ec9b4916f685
Job85 <julianjernigan@ymail.com>
2022-05-06 15:30:35 -0400

    created file, added logic GetWallet

---
+++ backend/controllers/WalletController.js
@@ -0,0 +1,15 @@
+const { Wallet } = require('../models')
+
+const GetWallet = async (req, res) => {
+    try {
+        const wallet = await Wallet.findAll()
+        console.log(wallet)
+        res.send(wallet)
+    } catch (error) {
+        throw error
+    }
+}
+
+module.exports = {
+    GetWallet
+}
