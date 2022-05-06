e839859d55f78c207b3591c2d6e85972f0020743
Job85 <julianjernigan@ymail.com>
2022-05-06 18:15:34 -0400

    migrated table update

--- backend/migrations/20220506214959-update-user-table.js
+++ backend/migrations/20220506214959-update-user-table.js
@@ -2,40 +2,12 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    const transaction = await queryInterface.sequelize.transaction();
-    try {
-      await queryInterface.addColumn(
-        'User',
-        'locationId',
-        {
-          type: Sequelize.DataTypes.INTEGER
-        },
-        { transaction }
-      );
-      await queryInterface.addIndex(
-        'User',
-        'locationId',
-        {
-          fields: 'locationId',
-          unique: true,
-          transaction,
-        }
-      );
-      await transaction.commit();
-    } catch (err) {
-      await transaction.rollback();
-      throw err;
-    }
+    await queryInterface.addColumn('users', 'locationId', {
+      type: Sequelize.INTEGER
+    });
   },
 
   down: async (queryInterface, Sequelize) => {
-    const transaction = await queryInterface.sequelize.transaction();
-    try {
-      await queryInterface.removeColumn('User', 'locationId', { transaction });
-      await transaction.commit();
-    } catch (err) {
-      await transaction.rollback();
-      throw err;
-    }
+    await queryInterface.removeColumn('users', 'locationId');
   }
 };
