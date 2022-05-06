a40c758c6e7b2b0a6aa7cd5246329875a91717c5
Job85 <julianjernigan@ymail.com>
2022-05-06 15:47:42 -0400

    added Wallet & User routes

--- backend/routes/AppRouter.js
+++ backend/routes/AppRouter.js
@@ -1,8 +1,12 @@
 const Router = require('express').Router()
 const TokenRouter = require('./TokenRouter')
 const LocationRouter = require('./LocationRouter')
+const WalletRouter = require('./WalletRouter')
+const UserRouter = require('./UserRouter')
 
 Router.use('/token', TokenRouter)
 Router.use('/location', LocationRouter)
+Router.use('/wallet', WalletRouter)
+Router.use('/user', UserRouter)
 
 module.exports = Router
