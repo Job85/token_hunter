ac57c5af63e7f6fa07d548bfb7fb425d9a02d310
Job85 <julianjernigan@ymail.com>
2022-05-06 16:57:45 -0400

    created demo seeds

--- backend/seeders/20220506162459-demo-locations.js
+++ backend/seeders/20220506162459-demo-locations.js
@@ -1,19 +1,39 @@
 'use strict';
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('locations', [{
+      latitude: 28.087692,
+      longitude: -82.486329,
+      diffLvl: 'Easy-Demo',
+      userId: 1,
+      createdAt: new Date(),
+      updateAt: new Date()
+    }, {
+      latitude: 27.963719,
+      longitude: -82.435767,
+      diffLvl: 'Medium-Demo',
+      userId: 1,
+      createdAt: new Date(),
+      updateAt: new Date()
+    }, {
+      latitude: 28.073568,
+      longitude: -82.373926,
+      diffLvl: 'Hard-Demo',
+      userId: 1,
+      createdAt: new Date(),
+      updateAt: new Date()
+    }, {
+      latitude: 28.088978,
+      longitude: -82.404310,
+      diffLvl: 'Medium-Demo',
+      userId: 1,
+      createdAt: new Date(),
+      updateAt: new Date()
+    }])
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     /**
      * Add commands to revert seed here.
      *
