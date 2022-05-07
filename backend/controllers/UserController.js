e134ece341837965c1d485b80b89af99b54e9858
Job85 <julianjernigan@ymail.com>
2022-05-07 15:34:22 -0400

    create function to post user

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,4 +1,5 @@
 const { User } = require('../models')
+const { post } = require('../routes/WalletRouter')
 
 const GetUser = async (req, res) => {
     try {
@@ -10,6 +11,17 @@ const GetUser = async (req, res) => {
     }
 }
 
+const CreateUser = async (req, res) => {
+    try {
+        const post = await post.create({ ...req.body })
+        console.log(post)
+        res.send(post)
+    } catch (error) {
+        throw error
+    }
+}
+
 module.exports = {
-    GetUser
+    GetUser,
+    CreateUser
 }
