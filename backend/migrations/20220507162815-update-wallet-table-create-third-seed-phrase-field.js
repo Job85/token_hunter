790362abaedd64a6de611bb3f10d256ebd81a3bd
Job85 <julianjernigan@ymail.com>
2022-05-07 12:35:02 -0400

    added logic for async up & async down

--- backend/migrations/20220507162815-update-wallet-table-create-third-seed-phrase-field.js
+++ backend/migrations/20220507162815-update-wallet-table-create-third-seed-phrase-field.js
@@ -1,21 +1,14 @@
 'use strict';
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn('wallets', 'seedPhrase3', {
+      type: Sequelize.STRING,
+      allowNull: false
+    })
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeColumn('wallets', 'seedPhrase3',)
   }
 };
