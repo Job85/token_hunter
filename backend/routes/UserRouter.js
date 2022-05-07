6b4894102684b52c76192f501d4b731ff642fdb1
Job85 <julianjernigan@ymail.com>
2022-05-07 15:31:04 -0400

    created route to post user

--- backend/routes/UserRouter.js
+++ backend/routes/UserRouter.js
@@ -2,5 +2,6 @@ const Router = require('express').Router()
 const controller = require('../controllers/UserController')
 
 Router.get('/', controller.GetUser)
+Router.post('/', controller.CreateUser)
 
 module.exports = Router
