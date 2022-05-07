68de838715045d37f5011f0e077d98349cc4d063
Job85 <julianjernigan@ymail.com>
2022-05-07 15:26:55 -0400

    created post wallet route

--- backend/routes/WalletRouter.js
+++ backend/routes/WalletRouter.js
@@ -2,5 +2,6 @@ const Router = require('express').Router()
 const controller = require('../controllers/WalletController')
 
 Router.get('/', controller.GetWallet)
+Router.post('/', controller.PostWallet)
 
 module.exports = Router
