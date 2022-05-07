23d0de520a162956f81419d1cafeba1fc2c58b58
Job85 <julianjernigan@ymail.com>
2022-05-07 15:30:07 -0400

    changed route for post wallet

--- backend/routes/WalletRouter.js
+++ backend/routes/WalletRouter.js
@@ -2,6 +2,6 @@ const Router = require('express').Router()
 const controller = require('../controllers/WalletController')
 
 Router.get('/', controller.GetWallet)
-Router.post('/', controller.PostWallet)
+Router.post('/', controller.CreateWallet)
 
 module.exports = Router
