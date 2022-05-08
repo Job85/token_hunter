2f2285c3fbdfec7745820f3c39ded66e7d9a2832
Job85 <julianjernigan@ymail.com>
2022-05-07 21:01:01 -0400

    changed to one demo token

--- backend/seeders/20220506155454-demo-tokens.js
+++ backend/seeders/20220506155454-demo-tokens.js
@@ -8,36 +8,6 @@ module.exports = {
       walletId: 1,
       createdAt: new Date(),
       updatedAt: new Date()
-    }, {
-      code: 'T3$T C0D3',
-      locationId: 2,
-      walletId: 1,
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }, {
-      code: '3V47 C0D3',
-      locationId: 3,
-      walletId: 1,
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }, {
-      code: 'D3M0 C0D3',
-      locationId: 1,
-      walletId: 2,
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }, {
-      code: 'T3$T C0D3',
-      locationId: 2,
-      walletId: 2,
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }, {
-      code: 'D3M0 C0D3',
-      locationId: 1,
-      walletId: 3,
-      createdAt: new Date(),
-      updatedAt: new Date()
     }])
   },
 
