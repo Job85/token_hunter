facb2f89b3421044aa0b3dd21c773768087fd114
Job85 <julianjernigan@ymail.com>
2022-05-08 01:54:41 -0400

    snaked_cased foriegn key id's

--- backend/seeders/20220506155454-demo-tokens.js
+++ backend/seeders/20220506155454-demo-tokens.js
@@ -4,8 +4,8 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('tokens', [{
       code: 'D3M0 C0D3',
-      locationId: 1,
-      walletId: 1,
+      location_id: 1,
+      wallet_id: 1,
       createdAt: new Date(),
       updatedAt: new Date()
     }])
