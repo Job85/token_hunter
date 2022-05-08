f760e0bc6a4853c6d74ce898c7f404ffccf22a73
Job85 <julianjernigan@ymail.com>
2022-05-07 21:53:58 -0400

    added route for post location

--- backend/routes/LocationRouter.js
+++ backend/routes/LocationRouter.js
@@ -2,5 +2,6 @@ const Router = require('express').Router()
 const controller = require('../controllers/LocationController')
 
 Router.get('/', controller.GetLocation)
+Router.post('/', controller.CreateLocation)
 
 module.exports = Router
