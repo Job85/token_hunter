5ef08ff10b571adcc566b72707b8ac20eee0f44c
Job85 <julianjernigan@ymail.com>
2022-05-07 21:00:20 -0400

    changed to one demo location

--- backend/seeders/20220506162459-demo-locations.js
+++ backend/seeders/20220506162459-demo-locations.js
@@ -10,30 +10,6 @@ module.exports = {
       tokenId: 1,
       createdAt: new Date(),
       updateAt: new Date()
-    }, {
-      latitude: 27.963719,
-      longitude: -82.435767,
-      diffLvl: 'Medium-Demo',
-      userId: 1,
-      tokenId: 2,
-      createdAt: new Date(),
-      updateAt: new Date()
-    }, {
-      latitude: 28.073568,
-      longitude: -82.373926,
-      diffLvl: 'Hard-Demo',
-      userId: 1,
-      tokenId: 3,
-      createdAt: new Date(),
-      updateAt: new Date()
-    }, {
-      latitude: 28.088978,
-      longitude: -82.404310,
-      diffLvl: 'Medium-Demo',
-      userId: 1,
-      tokenId: 4,
-      createdAt: new Date(),
-      updateAt: new Date()
     }])
   },
 
