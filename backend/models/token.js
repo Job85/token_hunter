8b322bf7960050bd4069948ba272079bd334b812
Job85 <julianjernigan@ymail.com>
2022-05-14 22:45:56 -0400

    added package json

--- backend/models/token.js
+++ backend/models/token.js
@@ -3,29 +3,6 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class token extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  token.init({
-    code: DataTypes.STRING,
-    walletId: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'token',
-  });
-  return token;
-}; 'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
   class Token extends Model {
     /**
      * Helper method for defining associations.
